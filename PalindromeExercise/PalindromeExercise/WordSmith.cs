c63cca08450e3b2502ebf1a5d97686ee0810c3d0
Zach Padovano <zacharyaz10@gmail.com>
2023-05-18 17:16:38 -0700

    Palindrome checker

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            {
+                //reverse the word!!   
+                var reversed = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversed += word[i]; //reversed = reversed + word[i]
+                }
+
+                if (reversed == word)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
-    public class UnitTest1
+    public class UnitTest1 
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("hello",false)]
+        [InlineData("nun",true)]
+        [InlineData("kayak", true)]
+        [InlineData("ach", false)]
+        [InlineData("rotator", true)]
+        public void MyTest(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+            bool actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
